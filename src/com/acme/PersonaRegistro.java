29e82e6b872a89c948b0f54e07b87223cdb6c847
rodo0919 <rpalacio19@gmail.com>
2026-01-02 19:41:47 +0100

    Se cagregó de codigo inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,6 +4,7 @@ public record PersonaRegistro(String nombre, String apellidos) {
 
     @Override
     public String toString() {
+        System.out.println("Esta instruccion es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
