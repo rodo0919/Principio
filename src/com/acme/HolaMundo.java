ffd13c2b6ffb9767e45f23451afde65eb6f1b00e
rodo0919 <rpalacio19@gmail.com>
2026-01-02 19:31:32 +0100

    Se ha subido la clase Persona

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -4,6 +4,8 @@ public class HolaMundo {
     public static void main(String[] args) {
         Mensaje msg=new Mensaje();
         String nombre = msg.solicitaDatos("Escribe tu nombre: ");
-        msg.saludar(nombre);
+        String apellidos = msg.solicitaDatos("Escribe tu apellido: ");
+        Persona p1 = new Persona(nombre, apellidos);
+        msg.saludar(p1);
     }
 }
---
+++ src/com/acme/Persona.java
@@ -0,0 +1,32 @@
+package com.acme;
+
+public class Persona {
+    private String nombre, apellidos;
+
+    public Persona(String nombre, String apellidos) {
+        this.nombre =nombre;
+        this.apellidos=apellidos;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
