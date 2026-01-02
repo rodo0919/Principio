07fb1a565429dc04509474baabef6abb343950a0
rodo0919 <rpalacio19@gmail.com>
2026-01-02 20:09:33 +0100

    Se quitó la linea de codigo inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,8 +4,6 @@ public record PersonaRegistro(String nombre, String apellidos) {
 
     @Override
     public String toString() {
-        //RR se ha comentado la línea de impresión
-        //System.out.println("Esta instruccion es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
